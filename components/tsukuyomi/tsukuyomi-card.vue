a767175f203fd13c119d9336df2f33914fcd3265
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2020-11-22 10:44:37 +0900

    Update tsukuyomi-card.vue

--- components/tsukuyomi/tsukuyomi-card.vue
+++ components/tsukuyomi/tsukuyomi-card.vue
@@ -1,32 +1,97 @@
 <template>
-    <div>
-        <v-card>
-            <v-card-text>
-                あと<span class="display-1 text-h3 text--primary">{{remainDayCount}}</span>日
-                
-                <v-card-title>{{title}}</v-card-title>
-                <v-card-subtitle>{{date}}</v-card-subtitle>
-            </v-card-text>
-            <v-card-actions>
-                <v-btn :to="'/detail/' + date + '/' +title" text>詳細を確認</v-btn>
-            </v-card-actions>
+  <div v-if="!removeCompleted">
+    <v-card :elevation="elevation">
+      <div v-if="!removed">
+        <v-card-text>
+          あと<span class="display-1 text-h4 text--primary">{{
+            remainDayCount
+          }}</span
+          >日
+
+          <v-card-title>{{ title }}</v-card-title>
+          <v-card-subtitle>{{ date }}</v-card-subtitle>
+        </v-card-text>
+        <v-card-actions>
+          <!--<v-btn :to="'/detail/' + date + '/' +title" text>詳細を確認</v-btn>-->
+          <v-btn
+            icon
+            v-if="removable !== undefined"
+            @click="removeConfirm = true"
+          >
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+          <v-btn icon v-if="sharable" :to="'/share/' + date + '/' + title">
+            <v-icon>mdi-share-variant</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </div>
+      <div v-if="removed">
+        <v-card-text> 削除しました </v-card-text>
+      </div>
+
+      <!--Confirm Removing-->
+      <v-expand-transition>
+        <v-card
+          v-if="removeConfirm"
+          class="transition-fast-in-fast-out v-card--reveal"
+          style="height: 100%"
+        >
+          <v-card-text class="pb-0">
+            <span class="text-h6 text--primary">削除確認</span>
+            <v-card-title>{{ title }}</v-card-title>
+            <v-card-subtitle>{{ date }}</v-card-subtitle>
+            <span class="text--primary">このイベントを削除しますか？</span>
+          </v-card-text>
+          <v-card-actions class="pt-0">
+            <v-btn text @click="removeConfirm = false"> キャンセル </v-btn>
+            <v-btn text v-on:click="remove" class="red white--text">
+              削除
+            </v-btn>
+          </v-card-actions>
         </v-card>
-    </div>
+      </v-expand-transition>
+    </v-card>
+  </div>
 </template>
 <script>
 module.exports = {
-    props:["title", "date"],
-    data(){
-        return {
-            "remainDayCount":0
-        }
-    },
-    mounted(){
-        //Calclate how many days
-        const now = (new Date()).getTime();
-        const target = (new Date(this.date)).getTime();
+  props: ["title", "date", "removable", "sharable", "elevation"],
+  data() {
+    return {
+      remainDayCount: 0,
+      removeConfirm: false,
+      removed: false,
+      removeCompleted: false,
+    };
+  },
+  methods: {
+    remove() {
+      this.removeConfirm = false;
+      this.removed = true;
 
-        this.remainDayCount = Math.ceil((target - now) / (1000*60*60*24));
-    }
-}
+      //Remove from the DB
+      const db = new Dexie("Tsukuyomi_events");
+      db.version(1).stores({
+        events: "title",
+      });
+
+      db.events.delete(this.title);
+    },
+  },
+  mounted() {
+    //Calclate how many days
+    const now = new Date().getTime();
+    const target = new Date(this.date).getTime();
+
+    this.remainDayCount = Math.ceil((target - now) / (1000 * 60 * 60 * 24)) - 1;
+  },
+};
 </script>
+<style scoped>
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
