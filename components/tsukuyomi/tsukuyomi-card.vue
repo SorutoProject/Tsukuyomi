1f046d18eaa66a40fc08f316d7ea80ebfc68581f
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2020-11-23 00:37:25 +0900

    Update tsukuyomi-card.vue

--- components/tsukuyomi/tsukuyomi-card.vue
+++ components/tsukuyomi/tsukuyomi-card.vue
@@ -93,7 +93,7 @@ module.exports = {
     const now = new Date().getTime();
     const target = new Date(this.date).getTime();
 
-    this.remainDayCount = Math.ceil((target - now) / (1000 * 60 * 60 * 24));
+    this.remainDayCount = Math.ceil((target - now) / (1000 * 60 * 60 * 24)) - 1;
   },
 };
 </script>
