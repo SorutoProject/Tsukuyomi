e1a81ccf4f0dffb548d5b26a67f82d52b99a9af8
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2020-11-28 23:37:46 +0900

    Update timeline.vue

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -4,37 +4,52 @@
       <v-btn icon elevation="0" v-on:click="goBack">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-      {{$t("timeline.title")}}
+      {{ $t("timeline.title") }}
     </p>
     <!--loader-->
     <p class="text-center" v-if="isPending">
       <v-progress-circular indeterminate color="primary"></v-progress-circular>
     </p>
-    <v-timeline dense v-for="(event,index) in events" :key="event.title" v-if="events.length > 0 && !isPending">
+    <div v-if="events.length > 0 && !isPending">
+      <v-timeline dense v-for="(event, index) in events" :key="event.title">
         <!--show "today" if index==0-->
-        <v-timeline-item small v-if="index === 0" color="teal">{{$t("timeline.today")}}</v-timeline-item>
-        <v-timeline-item hide-dot class="text--secondary" v-if="event.diff > 0">{{event.diff}}{{$t("timeline.days")}}</v-timeline-item>
+        <v-timeline-item small v-if="index === 0" color="teal">{{
+          $t("timeline.today")
+        }}</v-timeline-item>
+        <v-timeline-item hide-dot class="text--secondary" v-if="event.diff > 0"
+          >{{ event.diff }}{{ $t("timeline.days") }}</v-timeline-item
+        >
         <v-timeline-item small>
-          <tsukuyomi-card :title="event.title" :date="event.date" elevation="0" sharable="true" removable="true"></tsukuyomi-card>
+          <tsukuyomi-card
+            :title="event.title"
+            :date="event.date"
+            elevation="0"
+            sharable="true"
+            removable="true"
+          ></tsukuyomi-card>
         </v-timeline-item>
-    </v-timeline>
-    <p v-if="events.length === 0 && !isPending" class="text-center text--secondary">
-      {{$t("timeline.noEventsFoundText")}}
+      </v-timeline>
+    </div>
+    <p
+      v-if="events.length === 0 && !isPending"
+      class="text-center text--secondary"
+    >
+      {{ $t("timeline.noEventsFoundText") }}
     </p>
   </div>
 </template>
 <script>
 module.exports = {
-  data(){
-      return{
-          events:[],
-          isPending:true
-      }
+  data() {
+    return {
+      events: [],
+      isPending: true,
+    };
   },
-  methods:{
-    goBack(){
+  methods: {
+    goBack() {
       router.go(-1);
-    }
+    },
   },
   mounted() {
     //Get events from DB
@@ -68,19 +83,20 @@ module.exports = {
 
         //add to diff(from previous item) to items
         for (let i = 0; i < eventsFiltered.length; i++) {
-        const now = new Date();
-            now.setHours(0, 0, 0);
-            const target = new Date(eventsFiltered[i].date);
-            target.setHours(0, 0, 0);
+          const now = new Date();
+          now.setHours(0, 0, 0);
+          const target = new Date(eventsFiltered[i].date);
+          target.setHours(0, 0, 0);
 
-            const remainDayCount = Math.ceil(
-              (target.getTime() - now.getTime()) / (1000 * 60 * 60 * 24)
-            );
-            eventsFiltered[i].remainDayCount = remainDayCount;
+          const remainDayCount = Math.ceil(
+            (target.getTime() - now.getTime()) / (1000 * 60 * 60 * 24)
+          );
+          eventsFiltered[i].remainDayCount = remainDayCount;
           if (i === 0) {
             eventsFiltered[i].diff = remainDayCount;
           } else {
-            eventsFiltered[i].diff = remainDayCount - eventsFiltered[i - 1].remainDayCount;
+            eventsFiltered[i].diff =
+              remainDayCount - eventsFiltered[i - 1].remainDayCount;
           }
         }
 
